b02bc963a8697e9ff3b5241cebd43216f1e52135
penne <penne0505pp@gmail.com>
2024-12-22 12:10:59 +0900

    build: 🔒️ 環境変数の追加

---
+++ lib/env/env.dart
@@ -0,0 +1,19 @@
+import 'package:envied/envied.dart';
+
+part 'env.g.dart';
+
+@Envied(path: '.env')
+abstract class Env {
+  @EnviedField(varName: 'GERA_CLIENT_CS_ENGINE_ID', obfuscate: true)
+  static String engineId = _Env.engineId;
+
+
+  @EnviedField(varName: 'GERA_CLIENT_CS_API_KEY', obfuscate: true)
+  static String apiKey = _Env.apiKey;
+
+  @EnviedField(varName: 'GERA_CLIENT_CONFIG_PATH', obfuscate: true)
+  static String cfgPath = _Env.cfgPath;
+
+  @EnviedField(varName: 'GERA_CLIENT_DOWNLOAD_PATH', obfuscate: true)
+  static String downloadPath = _Env.downloadPath;
+}
