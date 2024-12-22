d6ba74aa36e7137937d18ac6f13515c1398a42e4
penne <penne0505pp@gmail.com>
2024-12-22 09:58:04 +0900

    docs: 📝 いくつかのコメント追加

--- lib/src/get_episode_info.dart
+++ lib/src/get_episode_info.dart
@@ -7,6 +7,7 @@ import 'package:http/http.dart' as http;
 import 'dart:io';
 
 
+/// downloadPath must be a full path including the file name
 Future<String> downloadThumbnail(String url, String downloadPath) async {
   final response = await http.get(Uri.parse(url));
 
@@ -20,15 +21,24 @@ Future<String> downloadThumbnail(String url, String downloadPath) async {
 }
 
 Future<String> getEpisodeInfo(int episodeNumber) async {
+  // エピソードのURLを取得
   final String episodeUrl = await getEpisodeUrl(episodeNumber);
+
+  // エピソードのDOMを取得
   final http.Response response = await http.get(Uri.parse(episodeUrl));
   final Document htmlDocument = parse(response.body);
+
+  // エピソードのタイトルと説明を取得
   final String rawTitle = htmlDocument.querySelector('meta[property="og:title"]')?.attributes['content'] ?? '';
   final String title = rawTitle.split(' ')[1];
   final String description = htmlDocument.querySelector('meta[property="og:description"]')?.attributes['content'] ?? '';
+  
+  // エピソードのシンボルを取得
   final String thumbnailUrl = htmlDocument.querySelector('meta[property="og:image"]')?.attributes['content'] ?? '';
   const String thumbnailPath = 'C:/Users/penne/Downloads/thumbnail.jpg'; // シンボルの保存先(運用時は、固定pathに$seriesName + thumbnail.jpgとして保存する)
   await downloadThumbnail(thumbnailUrl, thumbnailPath);
+  
+  // エピソードの音声ファイルURLを取得
   final String audioUrl = await getAudioUrl(episodeUrl);
 
 
