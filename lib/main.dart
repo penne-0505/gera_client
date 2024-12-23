ce03aba37d43465803f7540737a7534efd34867d
penne <penne0505pp@gmail.com>
2024-12-23 18:48:14 +0900

    #

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:gera_client/src/download_episode.dart';
 import 'package:gera_client/src/get_episode_info.dart';
+import 'package:gera_client/model/episode_info.dart';
+import 'package:gera_client/src/manage_path.dart';
+import 'package:gera_client/env/env.dart';
+import 'package:gera_client/src/play_episode.dart';
 
 
 void main() {
   // runApp(MainApp());
-  downloadEpisode(221);
-  getEpisodeInfo(221).then((value) => print(value));
+  getEpisodeInfos().then((e) => print(e));
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   MainApp({super.key});
-  
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  List<EpisodeInfo> episodeInfos = [];
+
+
+  @override
+  void initState() {
+    super.initState();
+    _initialize();
+  }
+
+  Future<void> _initialize() async {
+    episodeInfos = await getEpisodeInfos();
+  }
+
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello, World!'),
+          child: Column(
+            children: [
+              ElevatedButton(
+                onPressed: () async {
+                  await downloadEpisodes(1, episodeInfos.length + 1);
+                },
+                child: Text('Download all episodes'),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: episodeInfos.length,
+                  itemBuilder: (context, index) {
+                    final episodeInfo = episodeInfos[index];
+                    return ListTile(
+                      title: Text(episodeInfo.title),
+                      subtitle: Text(episodeInfo.episodeNumber.toString()),
+                      onTap: () async {
+                        final episodePlayer = EpisodePlayer();
+                        episodePlayer.play(episodeInfo.downloadPath);
+                      },
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
